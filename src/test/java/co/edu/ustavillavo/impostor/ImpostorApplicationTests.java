15aabefa7d1f3b20c83858b22fc56147d1c493a3
diego <diegobarbosa090305@gmail.com>
2026-02-20 09:28:51 -0500

    added initial push of project

---
+++ src/test/java/co/edu/ustavillavo/impostor/ImpostorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ustavillavo.impostor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpostorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
